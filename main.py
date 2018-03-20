88d33889cf7a3bc76e2179a172cf82b59f1e939e
Dan Kiel <dannyj.kiel@gmail.com>
2018-03-20 17:40:58 -0400

    Numpy Files
    Added saving the data to numpy files for a significantly faster load time.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np, time; from scipy.special import expit; import idx_parser as cparser
+import os, sys, time, numpy as np, idx_parser as cparser
+from scipy.special import expit
 
 class Network(object):
 	def __init__(self):
@@ -57,37 +58,9 @@ class Network(object):
 
 
 
-def start():
-	np.set_printoptions(linewidth=175)#Print final numbers on one line
-	#np.set_printoptions(threshold=np.nan)#Print entire array
-	#np.get_printoptions()
-
+def train(train_images, train_labels, validate_images, validate_labels, test_images, test_labels, wmu_test_images, wmu_test_labels):
 	num_epoch = 1000
 	nn = Network()
-
-	#Load the data
-	train_images = cparser.idx("data/mnist/train_images")
-	train_labels = cparser.idx("data/mnist/train_labels")
-	test_images = cparser.idx("data/mnist/test_images")
-	test_labels = cparser.idx("data/mnist/test_labels")
-
-	#images come back as a 3d np array.
-	#reshpae the image data, normalize it(retypes to floats), and append 1's
-	shape = train_images.shape
-	train_images = (train_images.reshape(shape[0], 28*28))/255
-	train_images = np.concatenate([train_images,np.ones((shape[0],1))],axis=1)
-	print(train_images.dtype)
-	shape = test_images.shape
-	test_images = (test_images.reshape(shape[0], 28*28))/255
-	test_images = np.concatenate([test_images,np.ones((shape[0],1))],axis=1)
-	
-	#Create validation set
-	validate_images = train_images[50000:]
-	train_images = train_images[0:50000]
-	
-	validate_labels = train_labels[50000:]
-	train_labels = train_labels[0:50000]
-
 
 	loss = np.Inf
 	error=[]
@@ -127,6 +100,75 @@ def start():
 		else:
 			break
 
+def load_data():
+	try:
+		#Numpy files are much faster to load than the mnist data
+		#Try to load these first
+		train_images = np.load("data/nmpy/train_images.npy")
+		train_labels = np.load("data/nmpy/train_labels.npy")
+		test_images = np.load("data/nmpy/test_images.npy")
+		test_labels = np.load("data/nmpy/test_labels.npy")
+		wmu_test_images = np.load("data/nmpy/wmu_test_images.npy")
+		wmu_test_labels = np.load("data/nmpy/wmu_test_labels.npy")
+
+	except IOError as e1:
+		print(e1)
+		print("Numpy hasn't been written. Trying idx...")
+
+		try:
+			#Load the data from mnist
+			train_images = cparser.idx("data/mnist/train_images")
+			train_labels = cparser.idx("data/mnist/train_labels")
+			test_images = cparser.idx("data/mnist/test_images")
+			test_labels = cparser.idx("data/mnist/test_labels")
+			wmu_test_images = cparser.idx("data/wmu/wmu_test_images")
+			wmu_test_labels = cparser.idx("data/wmu/wmu_test_labels")
+
+			#images come back as a 3d np array.
+			#reshpae the image data, normalize it(retypes to floats), and append 1's
+			shape = train_images.shape
+			train_images = (train_images.reshape(shape[0], 28*28))/255
+			train_images = np.concatenate([train_images,np.ones((shape[0],1))],axis=1)
+			
+			shape = test_images.shape
+			test_images = (test_images.reshape(shape[0], 28*28))/255
+			test_images = np.concatenate([test_images,np.ones((shape[0],1))],axis=1)
+
+			#Make the mnpy directory if it doesn't exist.
+			if not os.path.exists("data/nmpy"):
+				os.makedirs("data/nmpy")
+
+			np.save("data/nmpy/train_images", train_images)
+			np.save("data/nmpy/train_labels", train_labels)
+			np.save("data/nmpy/test_images", test_images)
+			np.save("data/nmpy/test_labels", test_labels)
+			np.save("data/nmpy/wmu_test_images", wmu_test_images)
+			np.save("data/nmpy/wmu_test_labels", wmu_test_labels)
+
+		except IOError as e2:
+			print("Couldn't find file.", e2)
+			print("Exiting...")
+			sys.exit(1)
+	
+	#Create validation set
+	validate_images = train_images[50000:]
+	train_images = train_images[0:50000]
+	
+	validate_labels = train_labels[50000:]
+	train_labels = train_labels[0:50000]
+
+	return train_images, train_labels, validate_images, validate_labels, test_images, test_labels, wmu_test_images, wmu_test_labels
+
+
+
+
+def initialize():
+	np.set_printoptions(linewidth=175)#Print final numbers on one line
+	#np.set_printoptions(threshold=np.nan)#Print entire array
+	#np.get_printoptions()
+
+
 #I don't know how to run main in pdb so this is my solution
 if __name__ == "__main__":
-	start()
+	initialize()
+	train(*(load_data()))
